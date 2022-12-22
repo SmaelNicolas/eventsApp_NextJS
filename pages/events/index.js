ee568f35e6d0e62fc4e29c1bd9b10a100c9f8071
Smael Nicolas <nico.smael@gmail.com>
2022-12-22 11:59:11 -0300

    get static props and do map

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,7 +1,34 @@
-import React from "react";
+import Image from "next/image";
 
-function Index() {
-	return <div>Index Events</div>;
+function EventsPage({ data }) {
+	return (
+		<div>
+			<h1>Events Page</h1>
+			<div>
+				{data.map((place) => (
+					<a key={place.id} href={`/events/${place.id}`}>
+						<Image
+							src={place.image}
+							alt={place.title}
+							width={300}
+							height={150}
+						/>
+						<h2>{place.title}</h2>
+					</a>
+				))}
+			</div>
+		</div>
+	);
 }
 
-export default Index;
+export default EventsPage;
+
+export async function getStaticProps() {
+	const { events_categories } = await import("/data/data.json");
+
+	return {
+		props: {
+			data: events_categories,
+		},
+	};
+}
